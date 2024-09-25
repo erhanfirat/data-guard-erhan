2b0add54562502663d41b8a11fd0e8755f0dd8bb
Erhan FIRAT <erhan.firat@cytosmart.com>
2024-09-25 17:09:02 +0300

    about page styling

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="about flex flex-col g-5">
-    <h1 class="text-xl">About</h1>
-    <hr />
-    <a href="https://www.linkedin.com/in/erhanfirat/" target="_blank">Linked.in Profile</a>
-    <a href="https://github.com/erhanfirat" target="_blank">Github Profile</a>
-    <a href="https://github.com/erhanfirat/data-guard-erhan" target="_blank"
-      >Project's Github Link</a
-    >
+  <div class="about">
+    <h3 class="pb-3 mb-3">About</h3>
+    <div class="flex flex-col g-3">
+      <a href="https://www.linkedin.com/in/erhanfirat/" target="_blank">Linked.in Profile</a>
+      <a href="https://github.com/erhanfirat" target="_blank">Github Profile</a>
+      <a href="https://github.com/erhanfirat/data-guard-erhan" target="_blank"
+        >Project's Github Link</a
+      >
+    </div>
   </div>
 </template>
 
