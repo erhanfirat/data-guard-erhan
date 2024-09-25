d8f30abfb86b51ce8cac917dfb7902ea80b13b56
Erhan FIRAT <erhan.firat@cytosmart.com>
2024-09-25 13:41:44 +0300

    page renamings

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import RepoView from '../views/RepoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'repositories',
+      component: RepoView
     },
     {
       path: '/about',
